0888b7dd487e472dadd0abf8a68477f23e41d1f1
Pau Alarcón <pau.alarcon.b@gmail.com>
2019-05-27 13:41:51 +0300

    First working version

---
+++ src/main/scala/com/paualarco/launcher/test/PerformanceTest.scala
@@ -0,0 +1,41 @@
+package scala.com.paualarco.launcher.test
+
+import java.lang.System.{currentTimeMillis => currentMillis}
+
+import org.apache.log4j.{Level, Logger}
+
+import scala.com.paualarco.ResourcesPool.ss
+import scala.com.paualarco.SparkRegistry._
+
+class PerformanceTest(dbName: String, tableName: String) {
+  val LOG = Logger.getLogger(this.getClass)
+  LOG.setLevel(Level.INFO)
+  LOG.info(s"DbName: $dbName")
+  LOG.info(s"TableName: $tableName")
+
+  def noRegistryTest(sqlStatement: String, iterations: Int): Long = {
+    val startTime = currentMillis
+    LOG.info("Performance test with NO Registry")
+    LOG.info(s"$iterations iterations accessing and counting to the same hive data")
+    for (i <- 1 until iterations) {
+      val df = ss.sql(sqlStatement)
+      val hash = df.hashCode
+      val count = df.count()
+      LOG.info(s"It.$i, Hash: $hash, Count: $count")
+    }
+    currentMillis - startTime
+  }
+
+  def registryTest(sqlStatement: String, iterations: Int): Long = {
+    val startTime = currentMillis
+    LOG.info("Performance test with Registry")
+    LOG.info(s"$iterations iterations accessing and counting to the same hive data")
+    for (i <- 1 until iterations) {
+      val df = ss.sqlRegistry(sqlStatement)
+      val hash = df.hashCode
+      val count = df.countRegistry
+      LOG.info(s"It.$i, Hash(DFRegistry): $hash, Count registry: $count")
+    }
+    currentMillis - startTime
+  }
+}
